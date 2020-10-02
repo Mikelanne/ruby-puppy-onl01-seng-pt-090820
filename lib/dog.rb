c7b4b02397dc29409cb16ef4a28b86025d0e37f1
Mikelanne Wood <mikelanne.wood@gmail.com>
2020-10-02 18:25:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,24 @@
 # Add your code here
+require 'pry'
+
+class Dog 
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.print_all
+    binding.pry
+    #@@all.each do |name|
+     # puts "#{@name}"
+   # end
+ # end
+end 
